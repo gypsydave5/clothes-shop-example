a9ba2a586a52a579bfdc5417e13615b71c0e8104
David Wickes <dave.wickes@gmail.com>
2014-12-09 11:47:25 +0000

    Extracts products view into a partial. Shares data between cotroller(s) as a service

--- js/app.js
+++ js/app.js
@@ -3,3 +3,15 @@ var clothesShop = angular.module('clothesShop', [
   'ngResource',
   'shopControllers'
   ]);
+
+clothesShop.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/products', {
+        templateUrl: 'partials/products.html',
+        controller: 'productsController'
+      }).
+      otherwise({
+        redirectTo: '/products'
+      });
+}]);
--- js/productsController.js
+++ js/productsController.js
@@ -1,12 +1,13 @@
 var shopControllers = angular.module('shopControllers', []);
 
-shopControllers.controller('productsController', ['$scope', '$http',
+shopControllers.service('sharedProperties', function() {
+  var productFilter = "";
+});
 
+shopControllers.controller('productsController', ['$scope', '$http', 'sharedProperties',
   function($scope, $http){
     $http.get('mock_db/allProducts.json').success(function(data){
       $scope.searchResults = data;
     });
-
-  $scope.productFilter = "";
-
 }]);
+
