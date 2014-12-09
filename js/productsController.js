23578877960f8d27caa74f12aa4c0897fb7b52f6
David Wickes <dave.wickes@gmail.com>
2014-12-09 16:26:41 +0000

    Adds failing test for returning a basket subtotal

--- js/productsController.js
+++ js/productsController.js
@@ -8,33 +8,15 @@ shopControllers.service('shoppingCart', function() {
   var cart = [];
 
   return {
-
     get: function() {
       return cart;
     },
-
     add: function(new_product) {
-      var id = new_product["id"];
-      var match = false;
-      cart.forEach(function(item) {
-        if (item.id == id) {
-          item.quantity ++;
-          match = true;
-        }
-      });
-      if (!match) {
-        new_product.quantity = 1;
-        cart.push(new_product);
-      }
+      cart.push(new_product);
     },
-
     remove: function(product) {
-      var id = product["id"];
-      index = cart.findIndex(function(item) { item.id = id });
-      cart[index]["quantity"] --;
-      if (cart[index]["quantity"] < 1) {
-        cart.splice(index, 1)
-      }
+      index = cart.indexOf(product);
+      cart.splice(index, 1);
     }
   }
 });
@@ -45,13 +27,19 @@ shopControllers.controller('productsController', [
   'sharedProperties',
   'shoppingCart',
   function($scope, $http, sharedProperties, shoppingCart){
+
     $http.get('mock_db/allProducts.json').success(function(data){
       $scope.searchResults = data;
     });
 
     $scope.cart = shoppingCart.get();
+
     $scope.cart.add = function(item) {
       shoppingCart.add(item);
+    }
+
+    $scope.cart.remove = function(item) {
+      shoppingCart.remove(item);
     }
 }]);
 
--- test/client/basketSpec.coffee
+++ test/client/basketSpec.coffee
@@ -16,22 +16,25 @@ describe 'shoppingCart', ->
   it 'can have items added to it', ->
     item = { id: 1, name: "bob" }
     scope.cart.add(item)
-    expect(scope.cart).toEqual [{ id: 1, name: "bob", quantity: 1}]
+    expect(scope.cart).toEqual [{ id: 1, name: "bob"}]
 
-  it 'can have increase the items in it', ->
+  it 'can have different items in it', ->
     item1 = { id: 1, name: "bob" }
-    item2 = { id: 1, name: "bob" }
-    item3 = { id: 1, name: "bob" }
     item4 = { id: 2, name: "joe" }
     scope.cart.add(item1)
-    scope.cart.add(item2)
-    scope.cart.add(item3)
     scope.cart.add(item4)
-    expect(scope.cart).toEqual [{ id: 1, name: "bob", quantity: 3}, {id: 2, name: "joe", quantity: 1}]
+    expect(scope.cart).toEqual [{ id: 1, name: "bob"}, {id: 2, name: "joe"}]
 
   it 'can have items removed from it', ->
     item = { id: 1, name: "bob" }
     scope.cart.add(item)
-    scope.cart.add(item)
-    scope.cart.add(item)
     scope.cart.remove(item)
+    expect(scope.cart).toEqual []
+
+  it 'can return a total for the items in it', ->
+    item1 = { id: 1, name: "bob", price: 10.00 }
+    item4 = { id: 2, name: "joe", price: 20.00 }
+    scope.cart.add(item1)
+    scope.cart.add(item4)
+    expect(scope.cart.total).toEqual 30
+
